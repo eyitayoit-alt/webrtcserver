afc62a6a2ecd092aa6ac4f28de1b7ff486d4932f
eyitayoit <eyitayoit@gmail.com>
2023-08-28 13:40:43 +0100

    initial commit for webrtcserver

---
+++ app.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const express = require('express');
+const cors=require('cors')
+const app=express()
+app.use(cors())
+const server = require('http').createServer(app);
+const io = require('socket.io')(server,{
+    cors:{origin:'http://127.0.0.1/:5173'}
+});
+const PORT=3000||process.env.PORT  
+io.on('connection', (socket) => { 
+   console.log('Connected')
+   
+   socket.on('message',(message)=>{
+   
+    socket.broadcast.emit('message',message)
+    
+   })
+  
+   
+   socket.on('disconnect',()=>{
+    console.log( "Disconnected")
+   })
+   socket.on('room-message',(message, name)=>{
+     if(name){
+        socket.to(name).emit('room-message',message)
+        console.log(message)
+   }
+    else{
+      socket.broadcast.emit('room-message',message)
+    }     
+   })
+   
+   socket.on('join-room',(room)=>{
+    if(room){
+      socket.join(room)
+    }
+   })
+    
+    
+       
+       
+      
+    
+   })
+  
+   
+  
+
+
+ function error(err, req, res, next) {
+    // log it
+    if (!test) console.error(err.stack);
+  
+    // respond with 500 "Internal Server Error".
+    res.status(500);
+    res.send( 'Internal Server Error');
+  }
+app.use(error)  
+server.listen(3000,()=>{
+    console.log('listening on Port 3000')
+});
